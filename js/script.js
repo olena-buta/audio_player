b82e0365e6492bd3ebc4930cee005a5801471c7e
Olena Buta <olena.buta@gmail.com>
2020-07-27 15:50:16 +0300

    added: switch sonds, progress bar

--- js/script.js
+++ js/script.js
@@ -39,6 +39,55 @@ function pauseSong() {
   audio.pause();
 }
 
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e) {
+  // progress.value = (audio.currentTime / audio.duration) * 100;
+  // or
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = Number(clickX / width) * duration;
+}
+
 playBtn.addEventListener('click', () => {
   musicContainer.classList.contains('play') ? pauseSong() : playSong();
 });
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar 
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
